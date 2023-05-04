502e2d5c60a6bd241a78e1b43ca7fdce7197f542
Nikolai Laevskii <nikolai.laevskii@akvelon.com>
2023-05-05 00:52:39 +0200

    Add trigger workflows script

---
+++ tw.ps1
@@ -0,0 +1,4 @@
+New-Item -Name "trigger_workflows" -Value ((Get-Content trigger_workflows) / 1 + 1) -Force;
+git add .;
+git commit -m "trigger_workflows";
+git push origin (git rev-parse --abbrev-ref HEAD);
